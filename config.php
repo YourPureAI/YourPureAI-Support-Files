2ceb26daceb0a6b3dda3827630c4232efd8ffb12
YourPureAI <yourpureai@gmail.com>
2025-04-23 09:29:20 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+// Example config file for you php API script. Always keep it away from the main accessable part of your website.
+<?php
+
+// Database credentials
+define('DB_SERVER', 'abc.example.com'); // IP or FQDN name of the DB server
+define('DB_USERNAME', 'username'); // User name for the DB
+define('DB_PASSWORD', 'abcd12345'); // Password for the DB
+define('DB_NAME', 'dbname'); // Database name
+
+// Other global settings
+define('SITE_URL', 'https://example.com'); // Change to your site URL
+
+// API Definitions
+define('API_KEY', 'insertyourapikeyhere'); // Replace with your API key for your PHP script
+define('API_action_type', 'dbQueryResult'); // Keep this as it is, but you can change it to anything you want. But then you need to change it also in Your Pure AI assistant settings.
+
+?>
